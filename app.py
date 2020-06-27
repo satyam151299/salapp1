4d32edfe98dd33f76cfcc1dc79143c33b6b73316
satyam151299 <62535402+satyam151299@users.noreply.github.com>
2020-06-27 21:14:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,19 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    int_features = [x for x in request.form.values()]
+    final_features=[]
+    for i in range(len(int_features)):
+        if(i>=2):
+            final_features.append(int(int_features[i]))
+    print(final_features)  
+    final_fe = [np.array(final_features)]
+    print(final_fe)
+    prediction = model.predict(final_fe)
 
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
